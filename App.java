d92ed309107a52875a6ca1facf0ebc7b4a24187f
Brenda Pereira <brendacamara@outlook.com>
2023-04-02 17:02:39 -0300

    adicionar lógica de rodadas

--- App.java
+++ App.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import javax.crypto.Mac;
+import java.util.Scanner;
 
 /**
  * - Ler arquvio e armazenar numero de rodadas
@@ -15,23 +19,39 @@ import javax.crypto.Mac;
  */
 
 class App {
-    public static void main(String args[]) {
-        ArrayList<Integer> listaCocos = new ArrayList<Integer>(Arrays.asList(0, 1, 2));
-        ArrayList<Integer> listaCocos2 = new ArrayList<Integer>(Arrays.asList(5,6,7, 1, 2));
+    public static void main(String args[]) throws FileNotFoundException {
+        ArrayList<Integer> listaCocos = new ArrayList<Integer>(Arrays.asList(0, 1, 2,4,4,5,6,7,2,1));
+        ArrayList<Integer> listaCocos2 = new ArrayList<Integer>(Arrays.asList(5, 6, 7, 1, 2,2,3,4,3,1,1,1,2,4,4,5,6,8,9));
         ArrayList<Integer> listaCocos3 = new ArrayList<Integer>(Arrays.asList(9, 2, 4, 2));
-        
+
         Macaco teste = new Macaco(2, 1, listaCocos);
         Macaco teste2 = new Macaco(0, 2, listaCocos2);
         Macaco teste3 = new Macaco(1, 0, listaCocos3);
-        
         ArrayList<Macaco> listaMacacos = new ArrayList<Macaco>(
                 Arrays.asList(teste, teste2, teste3));
+        String FILE_PATH = "arquivoTeste.txt";
+
+        File file = new File(FILE_PATH);
+
+        try (Scanner sc = new Scanner(file)) {
+
+            int rodadas = Integer.parseInt(sc.nextLine().replaceAll("\\D+", ""));
+
+            System.out.println(rodadas);
+            /*
+             * while (sc.nextLine() != null) {
+             * System.out.println(sc.nextLine());
+             * }
+             */
+
+            for (int i = 0; i < rodadas; i++) {
+                for (int j = 0; j < listaMacacos.size(); j++) {
+                    listaMacacos.get(j).arremessaCoco(listaMacacos);
+                }
+            }
+        }
 
         teste2.arremessaCoco(listaMacacos);
-
-        for (int i = 0; i < listaMacacos.size(); i++) {
-            listaMacacos.get(i).arremessaCoco(listaMacacos);
-        }
 
         System.out.println(teste.cocos.toString());
         System.out.println(teste2.cocos.toString());
